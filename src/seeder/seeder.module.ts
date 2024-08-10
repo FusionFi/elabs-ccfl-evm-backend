6edfbc485f84e4fdf73ecd1d07709b35c07dba74
congle-engineer <congle-engineer@gmail.com>
2024-08-10 22:14:55 +0700

    Change seed

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeederService } from './seeder.service';
 import { UserModule } from 'src/user/user.module';
 import { DbModule } from 'src/db/db.module';
+import { AdminModule } from 'src/admin/admin.module';
+import { User } from 'src/user/entity/user.entity';
+import { Network } from 'src/admin/entity/network.entity';
 
 @Module({
   providers: [SeederService],
-  imports: [UserModule, DbModule],
+  imports: [
+    TypeOrmModule.forFeature([User, Network]),
+    UserModule,
+    DbModule,
+    AdminModule
+  ],
 })
 export class SeederModule {}
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,28 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { User } from 'src/user/entity/user.entity';
+import { Network } from 'src/admin/entity/network.entity';
 import { UserService } from 'src/user/user.service';
 import { Role } from 'src/role/role.enum';
 import { ConfigService } from 'src/config/config.service';
+// import { AdminService } from 'src/admin/admin.service';
+import * as bcrypt from 'bcrypt';
 import 'dotenv/config';
 
 @Injectable()
 export class SeederService {
-  constructor(private userService: UserService) {}
+  private readonly logger = new Logger(SeederService.name);
+
+  constructor(
+    // private userService: UserService
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+
+    @InjectRepository(Network)
+    private networkRepository: Repository<Network>,
+  ) {}
 
   async seed() {
     try {
-      const users: User[] = [];
-      const userData = new User();
-      userData.firstName = 'admin';
-      userData.lastName = 'admin';
-      userData.username = 'admin';
-      userData.password = ConfigService.Admin.password;
-      userData.role = Role.Admin;
-      userData.email = 'admin@gmail.com';
-      userData.emailVerified = true;
-      userData.isActive = true;
-      users.push(await this.userService.signUp(userData));
-      return users;
-    } catch (e) {}
+      // const users: User[] = [];
+      const user = new User();
+      user.firstName = 'admin';
+      user.lastName = 'admin';
+      user.username = 'admin';
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(ConfigService.Admin.password, salt);
+      user.role = Role.Admin;
+      user.email = 'admin@gmail.com';
+      user.emailVerified = true;
+      user.isActive = true;
+
+      const existUser = await this.userRepository.findOneBy({
+        username: user.username,
+      });
+      if (!existUser) {
+        await this.userRepository.save(user);
+      }
+
+      let network = new Network();
+      network.name = "Sepolia";
+      network.code = "ETH_TESTNET_SEPOLIA";
+      network.chainId = "11155111";
+      network.txUrl = "https://sepolia.etherscan.io/tx/__TX_HASH__";
+      network.rpcUrl = "https://1rpc.io/sepolia";
+      network.isActive = true;
+
+      let existNetwork = await this.networkRepository.findOneBy({
+        chainId: network.chainId,
+      });
+      if (!existNetwork) {
+        await this.networkRepository.save(network);
+      }
+      
+      // users.push(await this.userService.signUp(userData));
+      // return users;
+    } catch (e) {
+      this.logger.error("[Seeder] Failed with error: ", e);
+    }
   }
 }
