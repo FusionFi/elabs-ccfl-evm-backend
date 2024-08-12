14ff52a85ee82545f0f939b08c4a98898e97b030
congle-engineer <congle-engineer@gmail.com>
2024-08-12 17:23:06 +0700

    Add scripts to test contract

---
+++ scripts/get-latest-price.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'ethers';
+import * as fs from 'fs';
+
+const abi = JSON.parse(fs.readFileSync('./abi/CCFL.json', 'utf8'));
+
+const contractAddress = '0x03aEf0CffcD9EdDE95692bF262B24E4240B19203';
+
+const providerURL =
+  'https://eth-sepolia.g.alchemy.com/v2/h_yCYWntvdt2dbI4vL2J-XjZnQk0jYnl';
+
+const provider = new ethers.JsonRpcProvider(providerURL);
+
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+async function getContractData() {
+  try {
+    const data = await contract.getLatestPrice("0x29f2D40B0605204364af54EC677bD022dA425d03", false);
+    console.log('Data:', data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+getContractData();
---
+++ scripts/withdraw.ts
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers';
+import * as fs from 'fs';
+
+const abi = JSON.parse(fs.readFileSync('./abi/CCFLPool.json', 'utf8'));
+
+const contractAddress = '0xD6483bb4aBEfc87812E5eb5a601Cfe70cD84F419';
+
+const providerURL =
+  'https://eth-sepolia.g.alchemy.com/v2/h_yCYWntvdt2dbI4vL2J-XjZnQk0jYnl';
+
+const provider = new ethers.JsonRpcProvider(providerURL);
+
+const privateKey = "0xac9b9103b72ac0281f5e82414478a54ebb4e6489195d072891168d74958141c6";
+const wallet = new ethers.Wallet(privateKey, provider);
+
+const contract = new ethers.Contract(contractAddress, abi, wallet);
+
+async function withdrawSupply() {
+  try {
+    const txResponse = await contract.withdraw(BigInt(10e6), {
+      gasLimit: BigInt(200000),
+    });
+    const txReceipt = await txResponse.wait();
+    console.log("Transaction successful:", txReceipt);
+  } catch (error) {
+    console.error("Error calling contract method:", error);
+  }
+}
+
+withdrawSupply();
