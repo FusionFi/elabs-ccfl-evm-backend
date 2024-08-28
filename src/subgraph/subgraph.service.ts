73de6188ab624d234e0f17c33431dbc154b8be56
congle-engineer <congle-engineer@gmail.com>
2024-08-28 13:10:11 +0700

    Support getting history for borrower via subgraph

--- src/subgraph/subgraph.service.ts
+++ src/subgraph/subgraph.service.ts
@@ -45,7 +45,7 @@ export class SubgraphService {
   async getHistory(address: string, offset: number, limit: number) {
     try {
       let history = [];
-      let allData = [];
+      const allData = [];
 
       const key = `getHistory_${address}`;
       const cacheData = await this.cacheManager.get(key);
@@ -56,7 +56,7 @@ export class SubgraphService {
           offset,
           limit,
           total: (cacheData as Array<any>).length,
-          history
+          history,
         };
       }
 
@@ -89,14 +89,6 @@ export class SubgraphService {
         }),
       };
 
-      let dataAddSupply = await axios(configAddSupply);
-      for (let item of dataAddSupply.data.data.addSupplies) {
-        allData.push({
-          type: 'add_supply',
-          ...item
-        })
-      }
-
       const configWithdrawSupply = {
         method: 'POST',
         url: ConfigService.Subgraph.url,
@@ -126,18 +118,229 @@ export class SubgraphService {
         }),
       };
 
-      let dataWithdrawSupply = await axios(configWithdrawSupply);
-      for (let item of dataWithdrawSupply.data.data.withdrawSupplies) {
+      const configCreateLoan = {
+        method: 'POST',
+        url: ConfigService.Subgraph.url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          query: `query createLoans (
+              $address: Bytes!
+          ) {
+              createLoans (
+                  orderBy: blockTimestamp
+                  orderDirection: desc
+                  where: {
+                      borrower: $address
+                  }
+              ) {
+                  borrower
+                  loanAmount
+                  supply
+                  collateralAmount
+                  collateral
+                  isYieldGenerating
+                  isETH
+                  loanId
+                  blockNumber
+                  blockTimestamp
+                  transactionHash
+              }
+          }`,
+          variables: { address: address },
+        }),
+      };
+
+      const configWithdrawLoan = {
+        method: 'POST',
+        url: ConfigService.Subgraph.url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          query: `query withdrawLoans (
+              $address: Bytes!
+          ) {
+              withdrawLoans (
+                  orderBy: blockTimestamp
+                  orderDirection: desc
+                  where: {
+                      borrower: $address
+                  }
+              ) {
+                  borrower
+                  loanId
+                  supply
+                  blockNumber
+                  blockTimestamp
+                  transactionHash
+              }
+          }`,
+          variables: { address: address },
+        }),
+      };
+
+      const configAddCollateral = {
+        method: 'POST',
+        url: ConfigService.Subgraph.url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          query: `query addCollaterals (
+              $address: Bytes!
+          ) {
+              addCollaterals (
+                  orderBy: blockTimestamp
+                  orderDirection: desc
+                  where: {
+                      borrower: $address
+                  }
+              ) {
+                  borrower
+                  loanId
+                  collateralAmount
+                  collateral
+                  isETH
+                  blockNumber
+                  blockTimestamp
+                  transactionHash
+              }
+          }`,
+          variables: { address: address },
+        }),
+      };
+
+      const configRepayLoan = {
+        method: 'POST',
+        url: ConfigService.Subgraph.url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          query: `query repayLoans (
+              $address: Bytes!
+          ) {
+              repayLoans (
+                  orderBy: blockTimestamp
+                  orderDirection: desc
+                  where: {
+                      borrower: $address
+                  }
+              ) {
+                  borrower
+                  loanId
+                  amount
+                  supply
+                  blockNumber
+                  blockTimestamp
+                  transactionHash
+              }
+          }`,
+          variables: { address: address },
+        }),
+      };
+
+      const configWithdrawAllCollateral = {
+        method: 'POST',
+        url: ConfigService.Subgraph.url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          query: `query withdrawAllCollaterals (
+              $address: Bytes!
+          ) {
+              withdrawAllCollaterals (
+                  orderBy: blockTimestamp
+                  orderDirection: desc
+                  where: {
+                      borrower: $address
+                  }
+              ) {
+                  borrower
+                  loanId
+                  isETH
+                  blockNumber
+                  blockTimestamp
+                  transactionHash
+              }
+          }`,
+          variables: { address: address },
+        }),
+      };
+
+      const [
+        dataAddSupply,
+        dataWithdrawSupply,
+        dataCreateLoan,
+        dataWithdrawLoan,
+        dataAddCollateral,
+        dataRepayLoan,
+        dataWithdrawAllCollateral,
+      ] = await Promise.all([
+        axios(configAddSupply),
+        axios(configWithdrawSupply),
+        axios(configCreateLoan),
+        axios(configWithdrawLoan),
+        axios(configAddCollateral),
+        axios(configRepayLoan),
+        axios(configWithdrawAllCollateral),
+      ]);
+
+      for (const item of dataAddSupply.data.data.addSupplies) {
+        allData.push({
+          type: 'add_supply',
+          ...item,
+        });
+      }
+
+      for (const item of dataWithdrawSupply.data.data.withdrawSupplies) {
         allData.push({
           type: 'withdraw_supply',
-          ...item
-        })
+          ...item,
+        });
+      }
+
+      for (const item of dataCreateLoan.data.data.createLoans) {
+        allData.push({
+          type: 'create_loan',
+          ...item,
+        });
+      }
+
+      for (const item of dataWithdrawLoan.data.data.withdrawLoans) {
+        allData.push({
+          type: 'withdraw_loan',
+          ...item,
+        });
+      }
+
+      for (const item of dataAddCollateral.data.data.addCollaterals) {
+        allData.push({
+          type: 'add_collateral',
+          ...item,
+        });
+      }
+
+      for (const item of dataRepayLoan.data.data.repayLoans) {
+        allData.push({
+          type: 'repay_loan',
+          ...item,
+        });
+      }
+
+      for (const item of dataWithdrawAllCollateral.data.data
+        .withdrawAllCollaterals) {
+        allData.push({
+          type: 'withdraw_all_collateral',
+          ...item,
+        });
       }
 
       allData.sort((a, b) => b.blockTimestamp - a.blockTimestamp);
-
       this.cacheManager.store.set(key, allData, ConfigService.Cache.ttl);
-      console.log('allData: ', allData);
 
       history = allData.slice(offset, offset + limit);
 
@@ -145,7 +348,7 @@ export class SubgraphService {
         offset,
         limit,
         total: allData.length,
-        history
+        history,
       };
     } catch (e) {
       throw new HttpException(e.response, e.status);
